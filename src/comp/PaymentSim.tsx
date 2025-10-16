6c898883790af009e637c143a9fa10ac8577d880
Koipix <koi.devt@gmail.com>
2025-10-17 05:27:20 +0800

    twk: auto nav after checkout

--- src/comp/PaymentSim.tsx
+++ src/comp/PaymentSim.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { useCart } from './useCart.ts'
+import { useNavigate } from 'react-router-dom'
 
 function PaymentSim() {
   const [processing, setProcessing] = useState(false);
   const { items, clearCart } = useCart();
   const total = items.reduce((sum, i) => sum + i.price * i.amount, 0);
+  const nav = useNavigate();
 
   const handlePayment = (e) => {
     e.preventDefault();
@@ -12,17 +14,20 @@ function PaymentSim() {
     setTimeout(() => {
       setProcessing(false);
       clearCart();
-      alert("Payment successful! 🎉");
+      alert("Payment successful! 🎉 Please wait for your order to arrive!");
+      nav("/")
     }, 1500);
   };
 
   return (
     <form onSubmit={handlePayment} className = "m-10 flex flex-col items-center border-2 py-7 gap-2">
-      <input className="border-1 p-2" placeholder="Card Number" required />
-      <input className="border-1 p-2" placeholder="MM/YY" required />
-      <input className="border-1 p-2" placeholder="CVV" required />
+      <input className="border-1 p-2" placeholder="Card Number"/>
+      <input className="border-1 p-2" placeholder="MM/YY"/>
+      <input className="border-1 p-2" placeholder="CVV"/>
       <h1 className="text-xl"> Total: {total} PHP</h1>
-      <button className="border-2 p-2 rounded-lg mt-2 w-50 font-semibold" type="submit">Pay Now</button>
+      <button className="border-2 p-2 rounded-lg mt-1 w-70 font-semibold" type="submit">Confirm Payment</button>
+      <button className="border-2 p-2 rounded-lg mt-1 w-70 font-semibold" type="submit">Pay with Gcash</button>
+      <button className="border-2 p-2 rounded-lg mt-1 w-70 font-semibold" type="submit">Cash on delivery</button>
     </form>
   )
 }
